a6d2f1377245e5bcf8e96791d214bda955974916
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-29 13:02:09 +0600

    js qestion

---
+++ java-script-qus.js
@@ -0,0 +1,64 @@
+// 1.What is Java Script ?
+
+// Ans : Java Script is a high lavel programming language.Java Script is a lightweight, interprated or just in time compailed programming language.Js can web site dynamic .
+
+// 2.How does java script Work?
+
+// Java script is programming language that used clint side and is one of the most effcient and widely used java scripting languages.
+
+// 3.What is variable ?
+
+// Variable are containers for storing value 
+// var name = 5;
+
+// 4.How declear a variable 
+
+// to creat a variable in js usuing key word and set a name usuing = operator and store a value . 
+
+// 5.Types of variablae ? How can you find out type of variable ?
+
+// 3 type of varibale 1- Number , 2- , String 3- Boolean
+// usuing typeof for find out type of variable 
+var name = 'Sajid';
+var type =typeof name;
+// console.log(type)
+
+// 6.Primitive and non primitive data type
+
+/* primitive data type  
+    1.Number
+    2.String
+    3.Boolean
+    4.Null
+    5.Undefinde
+*/
+/* Nom Primitive Data type
+    1.array
+    2.Object
+*/
+
+/* 7.Naming Convention of Js
+    1. Variable Name is case sensetive
+    2.Name can not keyword
+    3.Usuing Camel case // var myPresentAddress = 'Nandin';
+*/
+
+// 8. Math oprator 
+    /**
+     * +
+     * -
+     * * 
+     * /
+     * %
+    */
+//-----------/
+// 9.Short hand +=, -=, *=, /=;
+// 10.++ , --
+
+// 11. prsetInt or PerseFlot 
+/***
+ * Intefer usuing presInt
+ * Usuind Floting point number
+ * **/
+
+// 12.tofixed 
